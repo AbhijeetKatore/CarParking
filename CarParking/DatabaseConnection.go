d6fc99647e7aaacac43feb73235e17bb0830cad9
Abhijit Katore <abhijit_katore@persistent.com>
2022-10-19 10:42:42 +0530

    small change

--- CarParking/DatabaseConnection.go
+++ CarParking/DatabaseConnection.go
@@ -11,24 +11,22 @@ import (
 )
 
 func ConnectDatabase()( *mongo.Client,context.Context){
-client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
-if err != nil {
-	log.Fatal(err)
-}
-ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-err = client.Connect(ctx)
-if err != nil {
-	log.Fatal(err)
-}
-//defer client.Disconnect(ctx)
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//defer client.Disconnect(ctx)
 
 
-err = client.Ping(ctx, readpref.Primary())
-if err != nil {
-   log.Fatal(err)
-}
-
-
-return client,ctx
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+   	log.Fatal(err)
+	}
+	return client,ctx
 }
 
