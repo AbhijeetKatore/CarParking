5d03da1d0e91214e4e65071b91343390a2160332
Abhijit Katore <abhijit_katore@persistent.com>
2022-10-19 10:37:09 +0530

    Car deletion Done

--- CarParking/Car_Details.go
+++ CarParking/Car_Details.go
@@ -23,16 +23,37 @@ func AddCarDetails(){
 	client,ctx = ConnectDatabase()
 	collection := client.Database("CarParking").Collection("CarDetails")
 	doc := bson.D{{"Car Number", carNumber}, {"Car Model Name", carModel}}
-	
-	result, err := collection.InsertOne(ctx, doc)
+
+	_,err := collection.InsertOne(ctx, doc)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Inserted document with _id: %v\n", result.InsertedID)
+	fmt.Printf("Car Details Inserted Successfully")
 
 }
 
-func DeleteCarDetails(){}
+func DeleteCarDetails(){
+	fmt.Println("Enter Car Details to Delete")
+	fmt.Println("Enter Exact Car Number ")
+	fmt.Scanln(&carNumber)
+	fmt.Println("Enter Exact Car Model Name ")
+	fmt.Scanln(&carModel)
+
+	var client *mongo.Client 
+	var ctx context.Context
+	client,ctx = ConnectDatabase()
+	collection := client.Database("CarParking").Collection("CarDetails")
+	result, err := collection.DeleteMany(ctx, bson.D{{"Car Number", carNumber}, {"Car Model Name", carModel}})
+	if err != nil {
+    	log.Fatal(err)
+	}
+	if result.DeletedCount == 0{
+		fmt.Println("Data didn't Match to Delete")
+	}else{
+		fmt.Printf("Car Details Deleted Succesfully")
+	}
+
+}
 
 func UpdateCarDetails(){}
 
